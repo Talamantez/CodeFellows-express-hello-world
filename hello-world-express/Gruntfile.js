78882d0c25c58f7c51dfece5fc7ccdaf0274a139
Robert Talamantez <rtalamantez@localhost.localdomain>
2014-05-08 16:58:05 -0700

    casper-tests

--- hello-world-express/Gruntfile.js
+++ hello-world-express/Gruntfile.js
@@ -1,5 +1,43 @@
+'use strict';
+
 module.exports = function(grunt) {
+
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-express-server');
+    grunt.loadNpmTasks('grunt-casper');
+
+
     grunt.initConfig({
+        express: {
+            options: {
+                //Override defaults here
+            },
+            dev: {
+                options: {
+                    script: 'server.js'
+                }
+            },
+            prod: {
+                options: {
+                    script: 'server.js',
+                    node_env: 'production'
+                }
+            }
+        },
+        casper: {
+            acceptance: {
+                options: {
+                    test: true,
+                },
+                files: {
+                    'test/acceptance/casper-results.xml': ['test/acceptance/*_test.js']
+                }
+            }
+
+        },
+
         autoprefixer: {
             dist: {
                 files: {
@@ -13,9 +51,12 @@ module.exports = function(grunt) {
                 tasks: ['autoprefixer']
             }
         }
-    });
-    grunt.loadNpmTasks('grunt-autoprefixer');
-    grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('default', ['autoprefixer']);
+    });
+
+    grunt.registerTask('server', ['jshint', 'express:dev']);
+    grunt.registerTask('test', ['express:dev', 'casper']);
+    grunt.registerTask('autoprefixer', ['autoprefixer']);
+    grunt.registerTask('default', ['jshint', 'test']);
+
 };
